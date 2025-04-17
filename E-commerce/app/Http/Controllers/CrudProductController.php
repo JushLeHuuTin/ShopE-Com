72dfda8e7cfbf836ef739ffd4c4d948ef10d664b
JushLeHuuTin <jushlehuutinads@gmail.com>
2025-04-17 18:47:24 +0700

    tin update view product

--- E-commerce/app/Http/Controllers/CrudProductController.php
+++ E-commerce/app/Http/Controllers/CrudProductController.php
@@ -1,16 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
 
 
 use Illuminate\Http\Request;
 
 class CrudProductController extends Controller
 {
+    // public function index()
+    // {
+    //     // $featuredProducts = Product::where('is_featured', 1)->get();
+    //     // return view('welcome', compact('featuredProducts'));
+    // }
     public function index()
     {
-        // $featuredProducts = Product::where('is_featured', 1)->get();
-        // return view('welcome', compact('featuredProducts'));
+        $featuredProducts = Product::with('defaultVariant')
+        ->where('is_featured', 1)
+        ->get();
+        return view('index', compact('featuredProducts'));
     }
+
 
 }

91afa5efbc382af0ec285084d862c5d620a30ed4
xuantrieu64 <tvxt12@gmail.com>
2025-05-23 14:09:46 +0700

    trieu-merge

---
+++ E-commerce/resources/views/displayreview.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="card shadow-0 border" style="background: #f0f2f5">
+                    <div class="card-body p-4">
+                        <div class="comment-group">
+                            <div class="card mb-4">
+                                <div class="card-body">
+                                    <div class="user-name">User 1</div>
+                                    <p>San pham nay rat tot</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
